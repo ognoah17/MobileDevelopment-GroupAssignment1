292cca7cc0c099b3a2fd6b26738158904978edea
ognoah17 <noahjobse@gmail.com>
2025-01-18 18:45:38 -0700

    Update HeaderBar.tsx

--- SpotifyClone/components/HeaderBar.tsx
+++ SpotifyClone/components/HeaderBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { colors as Colors} from "../styles";
+import { colors as Colors } from "../styles";
 
 const HeaderBar = () => {
   return (
